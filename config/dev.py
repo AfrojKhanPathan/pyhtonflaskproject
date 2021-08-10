7bdfa88a1c219d2d1c1a953c070004d396a21bd1
AfrojKhanPathan <pafrojkhan@gmail.com>
2021-08-10 18:30:02 +0530

    add depencendy

--- config/dev.py
+++ config/dev.py
@@ -1,4 +1,4 @@
 DEBUG = True
-SECRET_KEY = 'topsecret',
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:topsecret@localhost/flask',
+SECRET_KEY = 'topsecret'
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@127.0.0.1/postgres'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
